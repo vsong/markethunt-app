a94d9fafd83d68368c9af768493c36ed5f181866
Victor Song <victor.xpsong@gmail.com>
2021-12-10 23:19:17 +0000

    fix dev environment setting var

--- config/setup.php
+++ config/setup.php
@@ -1,5 +1,5 @@
 <?php
-if ($_SERVER['APPDIR'] === 'dev') {
+if ($_SERVER['APPENV'] === 'dev') {
     ini_set('display_errors', '1');
     ini_set('display_startup_errors', '1');
     error_reporting(E_ALL);
