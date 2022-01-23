135ae24f79879d16285a73339be46f6b2f10c1f5
Victor Song <victor.xpsong@gmail.com>
2022-01-23 02:26:30 +0000

    moved mvp.php to index.php and prepare for prod launch

--- config/setup.php
+++ config/setup.php
@@ -1,5 +1,7 @@
 <?php
-if ($_SERVER['APPENV'] === 'dev') {
+define('APPENV', $_SERVER['APPENV'] ?? 'dev');
+
+if (APPENV === 'dev') {
     ini_set('display_errors', '1');
     ini_set('display_startup_errors', '1');
     error_reporting(E_ALL);
--- public/index.php
+++ public/index.php
@@ -1,3 +1,25 @@
 <?php
-header("Location: /mvp.php");
-exit();
+require_once($_SERVER['APPDIR'] . "/config/setup.php");
+require_once($_SERVER['APPDIR'] . "/model/ItemModel.php");
+
+$default_item_id = 114; //SUPER|brie+
+
+if (filter_input(INPUT_GET, 'item_id', FILTER_VALIDATE_INT)) {
+    $current_item_id = (int)$_GET['item_id'];
+    if (!isValidItemId($current_item_id)) {
+        $current_item_id = $default_item_id;
+    }
+} else {
+    $current_item_id = $default_item_id;
+}
+
+$current_item_name = getItemName($current_item_id);
+$stock_data = json_encode(getItemChartData($current_item_id));
+$stock_data = str_replace("},{", "},\n{", $stock_data); // make the html more readable
+
+echo $twig->render('index.html', [
+    'current_item_id' => $current_item_id,
+    'current_item_name' => $current_item_name,
+    'item_metadata' => getAllItemMetadata(),
+    'stock_data' => $stock_data
+]);
