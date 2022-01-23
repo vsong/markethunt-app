d047c1da8f400e7bb8617e54857452e223eb902f
Victor Song <victor.xpsong@gmail.com>
2022-01-23 03:28:08 +0000

    disable error reporting on non-dev instances

--- config/setup.php
+++ config/setup.php
@@ -5,6 +5,10 @@ if (APPENV === 'dev') {
     ini_set('display_errors', '1');
     ini_set('display_startup_errors', '1');
     error_reporting(E_ALL);
+} else {
+    ini_set('display_errors', 0);
+    ini_set('display_startup_errors', '0');
+    error_reporting(0);
 }
 
 require_once($_SERVER['APPDIR'] . '/vendor/autoload.php');
