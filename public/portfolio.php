39cab968aa57f63358d490379a79504ed146f19d
Victor Song <victor.xpsong@gmail.com>
2022-03-05 04:14:14 +0000

    MVP of portfolio functions

---
+++ public/portfolio.php
@@ -0,0 +1,23 @@
+<?php
+require_once($_SERVER['APPDIR'] . "/config/setup.php");
+require_once($_SERVER['APPDIR'] . "/model/ItemModel.php");
+
+$default_item_id = 114; //SUPER|brie+
+
+if (filter_input(INPUT_GET, 'item_id', FILTER_VALIDATE_INT)) {
+    $current_item_id = (int)$_GET['item_id'];
+    if (!isValidItemId($current_item_id)) {
+        $current_item_id = $default_item_id;
+    }
+} else {
+    $current_item_id = $default_item_id;
+}
+
+$current_item_name = getItemName($current_item_id);
+
+echo $twig->render('portfolio.html', [
+    'current_item_id' => $current_item_id,
+    'current_item_name' => $current_item_name,
+    'item_metadata' => getAllItemNamesAndLatestPrice(),
+    'initial_stock_data' => '{"success":true, "data":' . json_encode(getItemChartData($current_item_id)) . '}'
+]);
